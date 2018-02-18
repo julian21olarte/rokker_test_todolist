a9b11e3a6bbf7628936279d6dc550f31050faecf
julian21olarte <julian21olarte@gmail.com>
2018-02-18 16:36:11 -0500

    Uso de cors y limipeza de la estructura del proyecto.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var port = process.env.PORT || '3000';
+var cors = require('cors');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://julian21olarte:julian21olarte@ds245347.mlab.com:45347/rokker_labs_todolist', { useMongoClient: true })
@@ -22,25 +24,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//cors
+//app.use(cors({credentials: true, origin: 'http://localhost:4200'}));
+app.use(cors({credentials: true, origin: '*'}));
+
+
 //ROUTES
 app.use('/task', task);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+//server
+var server = app.listen(port, () => {
+  console.log("Server listening on " + port)
 });
+app.stop = function () {
+  server.close();
+}
 
 module.exports = app;
